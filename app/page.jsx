80211a110eb90768685af1c9d4f126cf204edbc9
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-04 11:55:37 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -9,12 +9,16 @@ export default function Home() {
       backgroundPosition: 'center',
       minHeight: '100vh',
     }}>
-      <Container sx={{ color: 'white', maxWidth: '600px', pt: 20 }}>
-        <Typography variant="h3" fontWeight="bold">
-          Painting & remodeling with craftsmanship you can trust
+      <Container sx={{ color: 'white', maxWidth: '600px', pt: 20, marginLeft: 40 }}>
+        <Typography variant="h2" fontWeight="bold" sx={{ marginTop: 20, whiteSpace: 'pre-line' }}>
+          {'Painting & remodeling \n with craftsmanship \n you can trust'}
         </Typography>
-        <Typography variant="body1" mt={3}>
-          We specialize in partnering with homeowners who appreciate exceptional craftsmanship and understand the value of a well-executed finish. If you’re ready to invest in lasting beauty and quality, we’d love to bring your vision to life.
+        <Typography variant="h5" mt={3} sx={{ whiteSpace: 'pre-line' }}>
+          {`We specialize in partnering with homeowners who \n`}
+          {`appreciate exceptional craftsmanship and understand \n`}
+          {`the value of a well-executed finish. If you're ready to \n`}
+          {`invest in lasting beauty and quality, we'd love to bring \n`}
+          {`your vision to life.`}
         </Typography>
       </Container>
     </Box>
