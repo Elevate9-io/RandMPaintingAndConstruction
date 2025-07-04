7d3813eaad2afe2a915a02d6465faaad1a4bd904
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-04 13:02:31 -0500

    Create FormField.jsx

---
+++ components/FormField.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+
+
+const FormField = styled(TextField)({
+  minWidth: '400px',
+  '& label': {
+    color: 'white',
+  },
+  '& label.Mui-focused': {
+    color: 'white',
+  },
+  '& .MuiInputBase-input': {
+    color: 'white',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'white',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'white',
+      borderRadius: '80px', // More rounded corners
+    },
+    '&:hover fieldset': {
+      borderColor: 'white',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'white',
+    },
+  },
+});
+
+
+export default FormField;
