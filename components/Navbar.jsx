a1a3b0f0212162f0ea5d389f90dec27cb823c1b5
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-04 11:40:46 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import Link from 'next/link';
 
+
+const pages = ['Home', 'About', 'Form', 'Contact'];
+
+
 const Navbar = () => (
   <AppBar position="static" color="transparent" elevation={0}>
-    <Toolbar sx={{ justifyContent: 'space-between' }}>
-      <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#747133' }}>
+    <Toolbar sx={{ justifyContent: 'space-between', marginLeft: 48, marginRight: 38 }}>
+      <Typography variant="h2" sx={{ fontWeight: 'bold', color: '#747133' }}>
         R&M
       </Typography>
-      <Box>
-        {['Home', 'About', 'Form', 'Contact'].map((page) => (
+      <Box sx={{ justifyContent: 'space-between', display: 'flex', width: '35%' }}>
+        {pages.map((page) => (
           <Link key={page} href={`/${page.toLowerCase()}`} passHref>
-            <Button sx={{ color: '#747133', fontWeight: 'bold', textTransform: 'none' }}>{page}</Button>
+            <Button sx={{ fontSize: 19, color: '#747133', fontWeight: 'bold', textTransform: 'none' }}>{page}</Button>
           </Link>
         ))}
       </Box>
