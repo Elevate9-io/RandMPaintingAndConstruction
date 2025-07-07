7716c7cafb7d8864b2afd668a5fb84a639856c4c
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-07 15:33:54 -0500

    Create R&M-Landingpage.jpg

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import { Box, Typography, Container } from '@mui/material';
 export default function Home() {
   return (
     <Box sx={{ position: 'relative', width: '100%', minWidth: 0, minHeight: '100vh', overflow: 'hidden' }}>
-      {/* Background Layer */}
       <Box
         sx={{
           position: 'fixed',
@@ -28,7 +27,6 @@ export default function Home() {
           },
         }}
       />
-      {/* Content Layer */}
       <Container
         sx={{
           color: 'white',
@@ -41,33 +39,35 @@ export default function Home() {
           boxSizing: 'border-box',
         }}
       >
-        <Typography
-          variant="h2"
-          fontWeight="bold"
-          sx={{
-            marginTop: 20,
-            whiteSpace: 'pre-line',
-            fontSize: { xs: '2rem', sm: '2.5rem', md: '3rem' },
-            lineHeight: 1.1,
-          }}
-        >
-          {'Painting & remodeling with craftsmanship you can trust'}
-        </Typography>
-        <Typography
-          variant="h5"
-          mt={3}
-          sx={{
-            whiteSpace: 'pre-line',
-            fontSize: { xs: '1rem', sm: '1.25rem', md: '1.5rem' },
-            lineHeight: 1.3,
-          }}
-        >
-          {`We specialize in partnering with homeowners who `}
-          {`appreciate exceptional craftsmanship and understand `}
-          {`the value of a well-executed finish. If you're ready to `}
-          {`invest in lasting beauty and quality, we'd love to bring `}
-          {`your vision to life.`}
-        </Typography>
+        <Box sx={{ width: '50%', pt: 10 }}>
+          <Typography
+            variant="h2"
+            fontWeight="bold"
+            sx={{
+              marginTop: 20,
+              whiteSpace: 'pre-line',
+              fontSize: { xs: '2rem', sm: '2.5rem', md: '3rem' },
+              lineHeight: 1.1,
+            }}
+          >
+            {'Painting & remodeling with craftsmanship you can trust'}
+          </Typography>
+          <Typography
+            variant="h5"
+            mt={3}
+            sx={{
+              whiteSpace: 'pre-line',
+              fontSize: { xs: '1rem', sm: '1.25rem', md: '1.5rem' },
+              lineHeight: 1.3,
+            }}
+          >
+            {`We specialize in partnering with homeowners who `}
+            {`appreciate exceptional craftsmanship and understand `}
+            {`the value of a well-executed finish. If you're ready to `}
+            {`invest in lasting beauty and quality, we'd love to bring `}
+            {`your vision to life.`}
+          </Typography>
+        </Box>
       </Container>
     </Box>
   );
