331dcfc8379bd42c30158bb089f42279d2c532bb
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-09 11:33:09 -0500

    Revert "Delete app/Contact directory"
    This reverts commit 5da23da003c4e1bc756b76346097b3e2c6ef5259.

---
+++ app/contact/page.jsx
@@ -0,0 +1,86 @@
+import { Box, Typography, Button, Grid } from '@mui/material';
+
+import FormField from 'components/FormField';
+
+
+export default function Form() {
+  return (
+    <Box sx={{ position: 'relative', width: '100%', minWidth: 0, minHeight: '100vh', overflow: 'hidden' }}>
+      <Box
+        sx={{
+          position: 'fixed',
+          top: { xs: '90px', sm: '90px' }, // offset for navbar height
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          backgroundImage: 'url("/images/background.jpg")',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          zIndex: -1,
+          '::after': {
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            bgcolor: 'rgba(0,0,0,0.4)',
+            zIndex: 1,
+          },
+        }}
+      />
+      <Box sx={{ pt: 20, ml: '20%', mr: '20%', position: 'relative', zIndex: 2 }}>
+        <Box>
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: 'bold',
+              color: 'white',
+              textAlign: 'center',
+              mt: 19,
+              mb: 4,
+              whiteSpace: 'pre-line'
+            }}
+          >
+            {`Contact us to \n`}
+            {`build your future`}
+          </Typography>
+        </Box>
+
+        <Box sx={{ width: '100%', pt: 7 }}>
+          <Grid container spacing={2} alignItems='stretch'>
+            <Grid item size={{ xs:12, md: 4 }}>
+              <FormField label="Full Name" />
+            </Grid>
+            <Grid item size={{ xs:12, md: 4 }}>
+              <FormField label="Email Address" />
+            </Grid>
+            <Grid item size={{ xs:12, md: 4 }}>
+              <FormField label="Phone Number" />
+            </Grid>
+            <Grid item size={{ xs:12, md: 12 }}>
+              <FormField label="Leave a comment" />
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box textAlign="center" mt={4}>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              backgroundColor: '#747133',
+              color: 'white',
+              width: 220,
+              height: '75px',
+              borderRadius: '25px',
+              fontSize: '1.6rem'
+            }}
+          >
+            SEND
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
