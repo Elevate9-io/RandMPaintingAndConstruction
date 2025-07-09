17f94eb0be35238529bffc6c72602a9d35e0b443
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-09 15:04:24 -0500

    Create send-email.js

---
+++ netlify/functions/send-email.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async function (event) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const { name, email, phone, comment } = JSON.parse(event.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // or use "smtp.mailgun.org", etc.
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.MAIL_TO, // client’s email
+    subject: `New contact from ${name}`,
+    text: `Phone number: ${phone} \n Comment: ${comment}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return { statusCode: 200, body: "Email sent" };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
