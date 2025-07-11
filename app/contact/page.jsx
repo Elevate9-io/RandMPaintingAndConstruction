ce9e666c9cb454cd8280fff32853b5e7caaed6c1
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-11 17:05:07 -0500

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Button, Grid } from '@mui/material';
+import { Box, Typography, Button, Grid, Alert } from '@mui/material';
 
 import FormField from 'components/FormField';
 
